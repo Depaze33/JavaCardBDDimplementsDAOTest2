de3ba1560270a636138e8a18f49631c55910f755
Clarisse Otmani <clarisseotm@gmail.com>
2024-10-13 14:29:25 +0200

    commentaire serialisation et déserialisation

--- src/main/java/fr/afpa/Deserializer.java
+++ src/main/java/fr/afpa/Deserializer.java
@@ -4,8 +4,21 @@ import java.util.ArrayList;
 
 public interface Deserializer<T> {
 //Methode to Deserializaiton
-    ArrayList<T> loadList(String filPath);
 
+    /**
+     * Deseriliser : lit le fichier et prend les infos serialiser et les transforme en une liste
+     *
+     * @param filePath chemin de fichier
+     * @return List Contact
+     */
+    ArrayList<T> loadList(String filePath);
+
+    /**
+     * Deserialiser (un seul contact) lit le fichier d'un seul contact, le transforme et le retourne
+     *
+     * @param filePath chemin de fichier
+     * @return un contact
+     */
     T load(String filePath);
 
 }
--- src/main/java/fr/afpa/Serializer.java
+++ src/main/java/fr/afpa/Serializer.java
@@ -5,8 +5,23 @@ import java.util.ArrayList;
 
 public interface Serializer<T> {
     //Methode for serialization
+
+    /**
+     * serialiser = enregistrer de la donnée dans un format particulier(vcard, Json, Binaire ...) dans un fochier ici filePath
+     *
+     * @param filePath     chemin vers un fichier
+     * @param objectToSave list
+     * @throws IOException
+     */
+    //filePath peux contenir les dossiers parents
     void saveList(String filePath, ArrayList<T> objectToSave) throws IOException;
 
+    /**
+     * Enregistre qu'un seul contact dans un format particulier
+     * @param filePath chemin vers un fichier
+     * @param objectToSave objet
+     * @throws IOException
+     */
     void save(String filePath, T objectToSave) throws IOException;
 
 }
